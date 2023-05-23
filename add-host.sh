e297cc83a59aee70a42f0982749cf4c4124b2bce
Exe303 <127712981+Exe303@users.noreply.github.com>
2023-05-24 06:10:58 +0700

    Add files via upload

--- add-host.sh
+++ add-host.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
+
 read -p "Silakan Masukan Domain Anda : " domainbaru
+
 #Validate
 if [[ $domainbaru == "" ]]; then
 echo "Please Input New Domain"
 exit 1
 fi
+
 #Input To Domain
 cat > /etc/v2ray/domain << END
 $domainbaru
 END
-systemctl stop v2ray
-systemctl stop v2ray@none
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
-systemctl start v2ray
-systemctl start v2ray@none
-echo Done
-sleep 0.5
+
 clear 
-neofetch
+echo "Succes Manambah Domain Baru"
+echo ""
+echo " Server Akan Reboot Otomatis"
+sleep 4
+echo ""
+reboot
