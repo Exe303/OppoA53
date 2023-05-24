c63ba0e709204f25f4375f693753981dd3a28044
Exe303 <127712981+Exe303@users.noreply.github.com>
2023-05-25 00:31:40 +0700

    Add files via upload

--- cert.sh
+++ cert.sh
@@ -9,14 +9,32 @@ echo start
 sleep 0.5
 source /var/lib/premium-script/ipvps.conf
 domain="$(cat /etc/v2ray/domain)"
-systemctl stop v2ray
-systemctl stop v2ray@none
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
-systemctl start v2ray
-systemctl start v2ray@none
-echo Done
-sleep 0.5
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/openvpn restart
+/etc/init.d/fail2ban restart
+/etc/init.d/cron restart
+/etc/init.d/nginx restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
+systemctl restart v2ray@none
+systemctl restart v2ray
+systemctl restart trojan
+systemctl restart v2ray@vless
+systemctl restart v2ray@vnone
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "          Service/s Restarted         "
+                echo -e ""
+                echo -e "======================================"
+                exit
 clear 
 neofetch
